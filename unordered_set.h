84aed824608f88570ac0bf180de9236fe8a02b7e
Sanket Kumar Singh <onlyforcodeforces@gmail.com>
2023-07-08 23:55:35 +0530

    Make unordered_set a header file and make remove a few bugs.

--- unordered_set.h
+++ unordered_set.h
@@ -1,7 +1,9 @@
+#pragma once
 #include <iostream>
 #include <forward_list>
 #include <string>
 #include <vector>
+#include <cstddef>
 template<typename T, typename Hash=std::hash<T>>
 class unordered_set {
 	public:
@@ -13,19 +15,20 @@ class unordered_set {
 		buffer = new typename std::forward_list<std::pair<std::size_t, T>>::iterator[cap];
 		buffer[0] = storage.end();
 		load_factor=0;
+		min_load_factor=0.25;
 		max_load_factor=1;
 		num_elements = 0;
 		last_element=storage.begin();
 	}
-	unordered_set(const unordered_set<T>& other) {
+	unordered_set(const unordered_set<T>& other): unordered_set()  {
 		//might not be the most efficient way should 'reserve' some space
 		//TODO implement 'reserve()' function.
-		for(auto it=other.begin();it!=other.end();it++)
+		for(auto &x: other)
 		{
-			insert(*it);
+			insert(x);
 		}
 	}
-	unordered_set(const std::vector<T>& init_list)
+	unordered_set(const std::vector<T>& init_list): unordered_set()
 	{
 		for (auto &x:init_list)
 		{
@@ -50,14 +53,16 @@ class unordered_set {
 		iterator& operator++()//prefix increment
 		{
 			//exceptions?
-			list_iterator++;
+			if(!list_iterator==storage.end())
+				list_iterator++;
 			return *this;
 		}
 		//postfix iterator
 		iterator operator++(int)
 		{
 			iterator tmp = *this;
-			list_iterator++;
+			if(!list_iterator==storage.end())
+				list_iterator++;
 			return tmp;
 		}
 		friend bool operator==(const iterator& a, const iterator& b)
@@ -76,8 +81,8 @@ class unordered_set {
 		typename std::forward_list<std::pair<std::size_t,T>>::iterator list_iterator;
 	};
 	void print() {
-		for(typename unordered_set<T>::iterator it = begin(); it!=end(); it++) {
-			std::cout<<*it<<" ";
+		for(auto &x:*this) {
+			std::cout<<x<<" ";
 		}
 		std::cout<<std::endl;
 	}
@@ -85,10 +90,11 @@ class unordered_set {
 		if (find(value)!=end())
 			return false;
 		if(num_elements+1>1ll*max_load_factor*cap) {
+			cap*=2;
 			rehash();
 		}
 		int hv = hasher(value);
-		int h = hv % cap;
+		int h = std::abs(hv) % cap;
 		if (buffer[h]!=storage.end()) {
 			storage.insert_after(buffer[h], {hv, value});
 		}
@@ -102,7 +108,6 @@ class unordered_set {
 		return true; //not handling error scenarios;
 	}
 	bool rehash() {
-		cap*=2;
 		typename std::forward_list<std::pair<std::size_t, T>>::iterator* new_buffer = new typename std::forward_list<std::pair<std::size_t, T>>::iterator[cap];	
 		std::forward_list<std::pair<std::size_t, T>> new_storage; 
 		new_storage.push_front({hasher(T()),T()});
@@ -113,7 +118,7 @@ class unordered_set {
 		//we need to make sure that "dummy" node value doesn't get copied in the rehashing process. Cost me hours :(.
 		for(;it!=storage.end();it++) {
 			auto element = *it;
-			int h = element.first%cap;
+			int h = std::abs(element.first)%cap;
 			if(new_buffer[h]!=new_storage.end())
 				new_storage.insert_after(new_buffer[h],{element.first, element.second});
 			else {
@@ -124,20 +129,22 @@ class unordered_set {
 		}
 		delete[] buffer;
 		buffer = new_buffer;
+		//storage is RAII so no need to explicitly "clear()" it.
 		storage = std::move(new_storage); //std::move is mandatory for retaining iterators.
 		load_factor = (num_elements*1.0/cap);
 		last_element = new_last_element;
 		return true; //not handling error scenarios;
 	}
 	
-	bool erase(int value) {
-		int h = hasher(value)%cap;		
+	bool erase(T value) {
+		//I should probably decrease the cap of the hash table if the num_elements < some threshold, look into this
+		int h = std::abs(hasher(value))%cap;		
 		auto it = buffer[h];
 		if (it == storage.end())
 			return false;
 		auto prev_it = it;
 		it++;
-		while(it!=storage.end() and it->first%cap==h) {
+		while(it!=storage.end() and std::abs(it->first)%cap==h) {
 			if(it->second == value) {
 				if (it == last_element) {
 					last_element = prev_it;
@@ -145,6 +152,10 @@ class unordered_set {
 				storage.erase_after(prev_it);
 				num_elements--;
 				load_factor = (num_elements*1.0/cap);
+				if (load_factor < min_load_factor) {
+					cap/=2;
+					rehash();
+				}
 				return true;
 			}
 			prev_it=it;
@@ -162,12 +173,12 @@ class unordered_set {
 		return iterator(storage.end());
 	}
 	iterator find(const T& value) {
-		int h = hasher(value)%cap;
+		int h = std::abs(hasher(value))%cap;
 		auto it = buffer[h];
 		if(buffer[h]==storage.end())
 			return end();
 		it++;
-		while(it!=storage.end() and it->first%cap==h) {
+		while(it!=storage.end() and std::abs(it->first)%cap==h) {
 			if(it->second == value) {
 				return it;
 			}
@@ -182,24 +193,7 @@ class unordered_set {
 	int cap;
 	double load_factor;
 	double max_load_factor;
+	double min_load_factor;
 	int num_elements;
 	Hash hasher;
 };
-
-int main()
-{
-	unordered_set<int> mp;
-	mp.insert(1);
-	mp.print();
-	mp.insert(2);
-	mp.print();
-	mp.insert(3);
-	mp.print();
-	mp.erase(2);
-	mp.print();
-	mp.insert(3);
-	mp.print();
-	mp.insert(10);
-	mp.print();
-	std::cout<<(mp.find(10)!=mp.end()?"found":"not found")<<std::endl;
-}
